79726b3871fd288f983c92f49585d5419ebd7fbe
rossanne <rossanneclamp@gmail.com>
2021-09-25 14:03:58 -0700

    remove footer, formated about and contact page

--- react-multi-page-website/src/components/Footer.jsx
+++ react-multi-page-website/src/components/Footer.jsx
@@ -3,13 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer">
-      <footer class="py-5 bg-dark fixed-bottom">
-        <div class="container">
-          <p class="m-0 text-center text-white">
-            
-          </p>
-        </div>
-      </footer>
+
     </div>
   );
 }
