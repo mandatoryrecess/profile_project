83066041358d3eaf07dfbc29790f394743cc425f
rossanne <rossanneclamp@gmail.com>
2021-11-20 11:23:53 -0800

    border added to contact box

--- react-multi-page-website/src/components/Contact.jsx
+++ react-multi-page-website/src/components/Contact.jsx
@@ -3,15 +3,11 @@ import "./Contact.css";
 
 function Contact() {
   return (
-    <div class="contact-container">
     <div>
-      <div class="wrapper" >
-        <h1>Contact</h1>
+    <div class="box c">
+      <div class="box a" >
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text
-          ever since the 1500s, when an unknown printer took a galley of
-          type and scrambled it to make a type specimen book.
+        rossanneclamp@gmail.coom
         </p>
       </div>
     </div>
