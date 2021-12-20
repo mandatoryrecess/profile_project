063cb5b8573f70b8aaa5ad41a399050bc98c7d63
rossanne <rossanneclamp@gmail.com>
2021-12-19 17:35:24 -0800

    body font added

--- react-multi-page-website/src/components/About.jsx
+++ react-multi-page-website/src/components/About.jsx
@@ -9,10 +9,7 @@ function About() {
         <div class="box text-box-red ">
           <h1>About</h1>
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book.
+          Web developer pivoting not-so-far from the Art world. My existing skillset in graphic design and photography lends itself well to front-end-focused web developing; I now have the knowledge and tools to bring a product together and to-life. I enjoy creating fluid designs that are intuitive to use.
           </p>
         </div>
     </div>
