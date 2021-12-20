45ea0040a3b49af5f01f19dbbf642abbdc2a46b3
rossanne <rossanneclamp@gmail.com>
2021-12-19 21:21:48 -0800

    hello

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,7 +9,7 @@ function Portfolio() {
       <div class="text-box-red ">
         <h1>Print Design</h1>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
+          Hello Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text
           ever since the 1500s, when an unknown printer took a galley of
           type and scrambled it to make a type specimen book.
