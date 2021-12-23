89cf806e80ac0c1eb7221087cfe9fd92b464ecfe
rossanne <rossanneclamp@gmail.com>
2021-12-22 17:50:42 -0800

    text and css added to portfolio page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,30 +1,27 @@
 import React from "react";
 import "./Portfolio.css";
 import "./Info.css";
-import largePrint from './LargePrint';
-import SmallPrint from './SmallPrint';
-import Digital from './Digital';
 import { Link, withRouter } from "react-router-dom";
 
-function Portfolio(props) {
+function Portfolio() {
   return (
   <div>
     <div>
       <div class="box-portfolio floating-slow">
-          <div class="floating-fast text-box-red ">
-            <Link  class=" " to="/largeprint">
-              <h1>Small Print</h1> 
+        <div class="portfolio-nav-cream">
+          <Link to="/digital">
+            <h1 class="portfolio-nav-text-box floating-fast">Digital</h1> 
+          </Link>
+        </div>
+          <div class="portfolio-nav-cream">
+            <Link to="/largeprint">
+              <h1 class="portfolio-nav-text-box floating-fast" >Large Print</h1> 
             </Link>
           </div>
    
-      <div class="floating text-box-red ">
-            <Link  class=" " to="/smallprint">
-              <h1>Large Print</h1> 
-            </Link>
-      </div>
-      <div class="floating-fast text-box-red ">
-            <Link  class=" " to="/digital">
-              <h1>Digital</h1> 
+      <div class="portfolio-nav-cream">
+            <Link  to="/smallprint">
+              <h1 class="portfolio-nav-text-box floating ">Small Print</h1> 
             </Link>
       </div>
      </div>
