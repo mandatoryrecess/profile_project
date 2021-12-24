ea2e1090da6d963540a9314f001fb713161beba2
rossanne <rossanneclamp@gmail.com>
2021-12-23 19:53:32 -0800

    SLRWrapper added

--- src/components/LargePrint.jsx
+++ src/components/LargePrint.jsx
@@ -3,7 +3,7 @@ import "./Portfolio.css";
 import "./Info.css";
 import SvgCircleArrowRight from "./SvgCircleArrowRight";
 import { Link, withRouter } from "react-router-dom";
-
+import { SRLWrapper } from "simple-react-lightbox";
 
 function LargePrint() {
   return (
@@ -11,13 +11,15 @@ function LargePrint() {
         <div class="box floating">       
           <div class="text-box-red ">
             <div >
-            <h1>LargePrint</h1>
-            <img src={"images/bridge_1.jpg"} height="100vw" />
-            <img src={"images/bridge2.jpg"} height="100vw"/>
-            <img src={"images/bus2013.jpg"} height="100vw"/>
-            <img src={"images/bus2016.jpg"} height="100vw"/>
-            <img src={"images/bus2019.jpg"} height="100vw"/>
-            <img src={"images/bus2016_2.jpg"} height="100vw"/>      
+              <h1>LargePrint</h1>
+              <SRLWrapper>
+                <img src={"images/bridge_1.jpg"} height="100vw" />
+                <img src={"images/bridge2.jpg"} height="100vw"/>
+                <img src={"images/bus2013.jpg"} height="100vw"/>
+                <img src={"images/bus2016.jpg"} height="100vw"/>
+                <img src={"images/bus2019.jpg"} height="100vw"/>
+                <img src={"images/bus2016_2.jpg"} height="100vw"/>   
+                </SRLWrapper>   
             </div>
           </div>
       </div>
